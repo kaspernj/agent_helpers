14a25146ea1cf2150fd2212f9eba128f2f884426
kaspernj <k@spernj.org>
2015-08-05 22:11:27 +0200

    Solved issue where helpers wouldn't get autoloaded

--- lib/agent_helpers.rb
+++ lib/agent_helpers.rb
@@ -2,15 +2,19 @@ require "agent_helpers/engine"
 require "string-cases"
 
 module AgentHelpers
+  # Solves issue when bundled as a dep the helpers wont get loaded. So don't remove this autoloader...
+  path = File.realpath(File.dirname(__FILE__) + "/../")
+  autoload :DetectorHelper, "#{path}/app/helpers/agent_helpers/detector_helper"
+
   def self.const_missing(name)
     filepath = "#{File.dirname(__FILE__)}/agent_helpers/#{::StringCases.camel_to_snake(name)}.rb"
-    
+
     if File.exists?(filepath)
       require filepath
       raise LoadError, "Still not defined: #{name}" unless ::AgentHelpers.const_defined?(name)
       return ::AgentHelpers.const_get(name)
     end
-    
+
     super
   end
 end
